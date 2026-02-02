a050cc60032202731e29a34d5e8f035826457332
Your Name <you@example.com>
2026-02-01 21:14:05 -0800

    Made unit test framework

---
+++ src/tests/test_template.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "helper_functions.hpp"
+#include "particle_filter.hpp"
+#include "state_functions.hpp"
+#include "thread_pool.hpp"
+
+TEST(TestTheTest, BasicBehavior) {
+    EXPECT_TRUE(true);
+}
